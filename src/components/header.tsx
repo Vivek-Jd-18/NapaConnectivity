1fff4d9f78b7d7c3679ce33eeafcc704148fc755
aaronc318 <aaronc171@gmail.com>
2022-08-10 07:23:13 -0400

    Added logo for header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div>
       <div>
-        <img src="" alt="main icon" />
+        <img src="https://napa-website.s3.ap-southeast-1.amazonaws.com/white-logo2.svg" alt="main icon" />
         <span>NAPA </span>
       </div>
       <div>
@@ -13,12 +13,12 @@ const Header = () => {
           <li>NAPA Society</li>
           <li>Social Art</li>
           <li>NFT Marketplace</li>
-          <li>NAPA Earn</li>
+          <li>NAPA Earn</li> ///only accessible post login///
         </ul>
       </div>
       <div>
         {/* <img/> */}
-        <Button>Log In</Button>
+        {/* <Button>Log In</Button> */}
         <Button> Connect Wallet</Button>
       </div>
     </div>
