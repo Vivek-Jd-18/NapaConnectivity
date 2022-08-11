08ffcf5c281c3b669bc9eaea6328a3787e55735d
hammadyounas <m.hammadyounas@outlook.com>
2022-08-11 20:15:14 +0500

    landing page section 2 completed

--- src/components/NapaSocietySection/NapaSocietySection.tsx
+++ src/components/NapaSocietySection/NapaSocietySection.tsx
@@ -1,10 +1,27 @@
 import Container from '@/Layout/Container/Container';
 import type { NextPage } from 'next';
+import { useState } from 'react';
 import Header from '../Header/Header';
 import Steper from '../Steper/Steper';
 import styles from './NapaSocietySection.module.scss';
 
 const NapaSociety: NextPage = () => {
+  const [slider, setSlider] = useState(1);
+
+  const handleIncrement = () => {
+    if (slider > 3) {
+      return;
+    }
+    setSlider(slider + 1);
+  };
+
+  const handleDecrement = () => {
+    if (slider < 2) {
+      return;
+    }
+    setSlider(slider - 1);
+  };
+
   return (
     <div className={styles.backgroundImage}>
       <Header />
@@ -22,15 +39,25 @@ const NapaSociety: NextPage = () => {
           </p>
         </div>
         <div className={styles.innerSocietyContainerRightSide}>
-          <h1 className={styles.heading3}>02</h1>
-
+          <h1 className={styles.heading3}>0{slider}</h1>
+          <div className={styles.sliderContainer}>
+            <div onClick={handleDecrement}>
+              <img src="/assets/images/left-arrow-white.svg" />
+            </div>
+            <div className={styles.sliderText}>
+              <p className={styles.sliderText1}>0{slider}</p>
+              <p className={styles.sliderText2}>/</p>
+              <p className={styles.sliderText2}>04</p>
+            </div>
+            <div onClick={handleIncrement}>
+              <img src="/assets/images/right-arrow-white.svg" />
+            </div>
+          </div>
           <h3 className={styles.communityHeading}>Community</h3>
           <h6 className={styles.description}>
-            Welcome to the NAPA Society experience! We are the face of crypto
-            social media in the web3 revolution! Join our society and turn your
-            social media into social art, mint your social art in our social
-            media app into NFT's and earn NAPA tokens, and buy and sell NFTs in
-            the largest marketplace in the world!
+            However venture pursuit he am mr cordial. Forming musical am hearing
+            studied be luckily. Ourselves for determine attending how led
+            gentleman sincerity.
           </h6>
           <div className={styles.joinSociety}>
             <button className={styles.joinSocietyBtn}>Join Society</button>
@@ -41,4 +68,5 @@ const NapaSociety: NextPage = () => {
     </div>
   );
 };
+
 export default NapaSociety;
