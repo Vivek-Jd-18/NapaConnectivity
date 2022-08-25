ddd51fabd2547c8c4a5b46ae6f5e885c23c8139d
hammadyounas <m.hammadyounas@outlook.com>
2022-08-25 18:39:55 +0500

    napa lounge size decreased

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -33,10 +33,9 @@ const walletButtonList = [
 ];
 
 const WalletComponent: NextPage = () => {
-
   const { push } = useRouter();
   const [account, setAccount] = useState('');
-  
+
   useEffect(() => {
     new Vivus('napa-logo', {
       type: 'delayed',
