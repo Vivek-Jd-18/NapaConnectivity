2d892ecc20428d83866a941a7b2ce9af9321bff2
hammadyounas <m.hammadyounas@outlook.com>
2022-08-29 15:16:41 +0500

    set up all toast messages

---
+++ src/typing/toast.ts
@@ -0,0 +1,25 @@
+export enum ToastTitle {
+  NEW_OFFER_MADE = 'New Offer Made',
+  NEW_INVITATION = 'New Invitation to Pool',
+  OFFER_ACCEPTED = 'Offer Accepted',
+  CHANGES_SAVED = 'Changes Saved',
+  ERROR = 'Error',
+  FAVORITE_NO_LONGER = 'Favorite No Longer Available',
+  WALLET_CONNECTED = 'Wallet Connected',
+  WALLET_DISCONNECTED = 'Wallet Disconnected',
+  WALLET_NEEDS_TO_CONNECTED = 'Wallet Needs to Connected',
+  WALLET_IS_ALREADY_CONNECTED = 'Wallet is Already Connected',
+}
+
+export enum ToastDescription {
+  NEW_OFFER_MADE = 'Clay Ortiz wants to exchange NFT.',
+  NEW_INVITATION = 'You are invited to the pool ”Infinite.',
+  OFFER_ACCEPTED = 'Your offer has been accepted.',
+  CHANGES_SAVED = 'Changes were successfully saved.',
+  ERROR = 'Unfortunately, an error occurred.',
+  FAVORITE_NO_LONGER = 'Unfortunately, the favorite "Illusions of..',
+  WALLET_CONNECTED = 'Your wallet has been successfully connected.',
+  WALLET_DISCONNECTED = 'Your wallet has been disconnected.',
+  WALLET_NEEDS_TO_CONNECTED = 'Please connect your wallet.',
+  WALLET_IS_ALREADY_CONNECTED = 'Your wallet has been already connected.',
+}
