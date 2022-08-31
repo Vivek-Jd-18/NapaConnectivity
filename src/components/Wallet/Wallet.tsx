f3dd8ac22e008e53d327f885cd5d76f6cf10a915
Tajinder Singh <88tajsingh@gmail.com>
2022-08-31 18:57:08 +0530

    latest css w/o sliders

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -1,31 +1,9 @@
 import { NapaLogo, NapaLogoWhite } from '../../components/Svg';
-
 import { useCallback, useEffect } from 'react';
-
 import Vivus from 'vivus';
 import Container from '../../Layout/Container/Container';
 import type { NextPage } from 'next';
 import styles from '../../../styles/pages/Wallet.module.scss';
-import { WalletConnectedIcon } from '../../components/assets';
-import { toast } from 'react-toastify';
-import { useRouter } from 'next/router';
-import { CustomToastWithLink } from '../CustomToast/CustomToast';
-import { walletButtonList } from '@/constants/wallet.constants';
-import Image from 'next/image';
-import { ToastDescription, ToastTitle } from '@/typing/toast';
-
-type WalletComponentProps = {
-  connectWallet: () => void;
-  account: string;
-};
-
-const WalletComponent: NextPage<WalletComponentProps> = ({
-  connectWallet,
-  account,
-}) => {
-  const { push } = useRouter();
-
-
 import {
   MetaMaskIcon,
   CoinbaseIcon,
@@ -60,7 +38,6 @@ const walletButtonList = [
 ];
 
 const WalletComponent: NextPage = () => {
-
   useEffect(() => {
     new Vivus('napa-logo', {
       type: 'delayed',
@@ -69,7 +46,6 @@ const WalletComponent: NextPage = () => {
       animTimingFunction: Vivus.EASE_OUT,
     });
   }, []);
-
 
   const connectWallet = useCallback(async () => {
     try {
@@ -96,31 +72,21 @@ const WalletComponent: NextPage = () => {
     }
   }, [getWeb3]);
 
-
   return (
     <div className={styles.backgroundImage}>
       <Container>
         <div
           className={`row justify-content-between align-items-center col-12 ${styles.innerMainContainer}`}
         >
-
-          <div className={`col-xl-6 col-md-12 d-flex justify-content-center`}>
-            <div className={styles.svgWrapper} onClick={() => push('/home')}>
-
           <div className="col-xl-6 col-md-12">
             <div className={styles.svgWrapper}>
-
               <NapaLogo
                 className={styles.napaLogo}
                 width={'360'}
                 height={'80'}
               />
             </div>
-
-            <div className={styles.svgWrapper} onClick={() => push('/')}>
-
             <div className={styles.svgWrapper}>
-
               <NapaLogoWhite
                 className={styles.napaLogoWhite}
                 width={'360'}
@@ -129,23 +95,14 @@ const WalletComponent: NextPage = () => {
             </div>
           </div>
           <div className="col-xl-6 col-md-12">
-
             <div className="flex-column">
               <h1 className={styles.walletText}>Select Wallet</h1>
-
-            <div className="flex-column mx-width">
-              <h1 className={styles.walletText}>Select a Wallet</h1>
-
               <span className={styles.descriptionText}>
                 By connecting your wallet, you agree to our{' '}
                 <span className={styles.termsPrivacyText}>
                   Terms of Service
                 </span>
-
                 &nbsp;and our{' '}
-
-                &nbsp;<br></br>and our{' '}
-
                 <span className={styles.termsPrivacyText}>Privacy Policy.</span>
               </span>
               <div className={`${styles.btnContainer} flex-column`}>
@@ -154,36 +111,12 @@ const WalletComponent: NextPage = () => {
                     <button
                       key={index}
                       onClick={() => {
-
-                        if (index === 0 && account) {
-                          toast.error(
-                            CustomToastWithLink({
-                              icon: WalletConnectedIcon,
-                              title: ToastTitle.WALLET_IS_ALREADY_CONNECTED,
-                              description:
-                                ToastDescription.WALLET_IS_ALREADY_CONNECTED,
-                              time: 'Now',
-                            })
-                          );
-                          return;
-                        }
-                        if (index === 0) {
-                          connectWallet();
-                        }
-
                         if (index === 0) connectWallet();
-
                       }}
                       className={styles.walletBtn}
                       style={{ borderColor: borderColor }}
                     >
-                      <Image
-                        src={icon}
-                        width={50}
-                        height={50}
-                        className={styles.btnIcon}
-                        alt={`${text}`}
-                      />
+                      <img src={icon} />
                       {text}
                     </button>
                   );
