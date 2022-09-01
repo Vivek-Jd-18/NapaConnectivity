e1fe254a802465f5e8945987321347f0c8d9afc3
hammadyounas <m.hammadyounas@outlook.com>
2022-09-01 21:50:30 +0500

    chat feature implemented

--- src/components/ChatWindow/ChatWindow.tsx
+++ src/components/ChatWindow/ChatWindow.tsx
@@ -1,100 +1,51 @@
 import type { NextPage } from 'next';
-import { HowardAvatar } from '../assets';
 import ChatListItemCard from '../ChatListItemCard/ChatListItemCard';
 import styles from './ChatWindow.module.scss';
 
-const chatList = [
-  {
-    avatar: HowardAvatar,
-    userName: 'Tom Bradley',
-    description:
-      'Abroad no chatty others my silent an. Fat way appear denote who wholly narrow gay settle.',
-  },
-  {
-    avatar: HowardAvatar,
-    userName: 'Tom Bradley',
-    description:
-      'By spite about do of do allow blush. Additions in conveying or collected objection in. Suffer few desire wonder her object hardly nearer. ',
-  },
-  {
-    avatar: HowardAvatar,
-    userName: 'Tom Bradley',
-    description: 'Change wholly say why eldest period.',
-  },
-  {
-    avatar: HowardAvatar,
-    userName: 'Tom Bradley',
-    description:
-      'By spite about do of do allow blush. Additions in conveying or collected objection in. Suffer few desire wonder her object hardly nearer. ',
-  },
-  {
-    avatar: HowardAvatar,
-    userName: 'Tom Bradley',
-    description:
-      'By spite about do of do allow blush. Additions in conveying or collected objection in. Suffer few desire wonder her object hardly nearer. ',
-  },
-  {
-    avatar: HowardAvatar,
-    userName: 'Tom Bradley',
-    description:
-      'By spite about do of do allow blush. Additions in conveying or collected objection in. Suffer few desire wonder her object hardly nearer. ',
-  },
-  {
-    avatar: HowardAvatar,
-    userName: 'Tom Bradley',
-    description:
-      'By spite about do of do allow blush. Additions in conveying or collected objection in. Suffer few desire wonder her object hardly nearer. ',
-  },
-  {
-    avatar: HowardAvatar,
-    userName: 'Tom Bradley',
-    description:
-      'By spite about do of do allow blush. Additions in conveying or collected objection in. Suffer few desire wonder her object hardly nearer. ',
-  },
-  {
-    avatar: HowardAvatar,
-    userName: 'Tom Bradley',
-    description:
-      'By spite about do of do allow blush. Additions in conveying or collected objection in. Suffer few desire wonder her object hardly nearer. ',
-  },
-  {
-    avatar: HowardAvatar,
-    userName: 'Tom Bradley',
-    description:
-      'By spite about do of do allow blush. Additions in conveying or collected objection in. Suffer few desire wonder her object hardly nearer. ',
-  },
-  {
-    avatar: HowardAvatar,
-    userName: 'Tom Bradley',
-    description:
-      'By spite about do of do allow blush. Additions in conveying or collected objection in. Suffer few desire wonder her object hardly nearer. ',
-  },
-  {
-    avatar: HowardAvatar,
-    userName: 'Tom Bradley',
-    description:
-      'By spite about do of do allow blush. Additions in conveying or collected objection in. Suffer few desire wonder her object hardly nearer. ',
-  },
-];
+type ChatWindowProps = {
+  message: string;
+  setMessage: (message: string) => void;
+  messageHandler: () => void;
+  messages: any;
+};
 
-const ChatWindow: NextPage = () => {
+const ChatWindow: NextPage<ChatWindowProps> = ({
+  message,
+  setMessage,
+  messageHandler,
+  messages,
+}) => {
   return (
     <>
       <div className={styles.chatWindowContainer}>
-        {chatList.map(({ userName, avatar, description }, index) => {
-          return (
-            <ChatListItemCard
-              key={`chat-${index}`}
-              username={userName}
-              avatar={avatar}
-              description={description}
-            />
-          );
-        })}
+        {messages &&
+          messages.length > 0 &&
+          messages.map(({ message }: any, index: number) => {
+            return (
+              <ChatListItemCard
+                key={`chat-${index}`}
+                username={message.from}
+                // avatar={avatar}
+                description={message.text}
+              />
+            );
+          })}
       </div>
       <div className={styles.chatInputContainer}>
-        <input className={styles.input} placeholder="Message.." />
-        <button className={styles.send}>Send</button>
+        <input
+          className={styles.input}
+          placeholder="Message.."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              messageHandler();
+            }
+          }}
+        />
+        <button className={styles.send} onClick={messageHandler}>
+          Send
+        </button>
       </div>
     </>
   );
---
+++ src/constants/url.ts
@@ -0,0 +1,2 @@
+export const WEBSOCKET_URL = 'ws://localhost:5000';
+export const API_URL = 'http://localhost:8080';
