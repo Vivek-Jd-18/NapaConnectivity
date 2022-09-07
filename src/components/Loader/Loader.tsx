29f6d857de3b7f619cd30df8663b0e66266d5999
mvtdev1050 <myvirtualteams1@gmail.com>
2022-09-07 14:46:50 +0530

    responsive and loader logo

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,26 +1,27 @@
 import type { NextPage } from 'next';
 import { useEffect } from 'react';
-import Vivus from 'vivus';
+// import Vivus from 'vivus';
 import { NapaLogo, NapaLogoWhite } from '../Svg';
 import styles from './Loader.module.scss';
 
 const Loader: NextPage = () => {
   useEffect(() => {
-    new Vivus('napa-logo', {
-      type: 'delayed',
-      duration: 200,
+    // new Vivus('napa-logo', {
+    //   type: 'delayed',
+    //   duration: 200,
 
-      animTimingFunction: Vivus.EASE_OUT,
-    });
+    //   animTimingFunction: Vivus.EASE_OUT,
+    // });
   }, []);
 
   return (
     <div className={styles.loaderContainer}>
       <div className={styles.svgWrapper}>
-        <NapaLogo className={styles.napaLogo} />
+        {/* <NapaLogo className={styles.napaLogo} /> */}
       </div>
       <div className={styles.svgWrapper}>
-        <NapaLogoWhite className={styles.napaLogoWhite} />
+        {/* <NapaLogoWhite className={styles.napaLogoWhite} /> */}
+        <h1 className={styles.loaderText} id="napa-logo" data-text="napa">napa</h1>
       </div>
     </div>
   );
