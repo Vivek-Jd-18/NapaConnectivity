448ab9c73c90b33bf3c78d7708ccb7b92e711a2f
aaronc318 <aaronc171@gmail.com>
2022-09-08 14:56:58 +0700

    fixed ws endpoint

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,2 +1,2 @@
-export const WEBSOCKET_URL = 'wss://napa-backend-staging.napasociety.io:5000';
+export const WEBSOCKET_URL = 'wss://napa-backend-staging.napasociety.io';
 export const API_URL = 'https://napa-backend-staging.napasociety.io';
