fd80b9526273b336c11e7294ee279d70f6a3b580
aaronc318 <aaronc171@gmail.com>
2022-09-12 12:09:50 +0700

    changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -67,52 +67,8 @@ const Header: NextPage<HeaderProps> = ({
       animTimingFunction: Vivus.EASE_OUT,
     });
   }, []);
-
-
-
-
-// scroll effect
-const body = document.body;
-// const triggerMenu = document.querySelector(".page-header .trigger-menu");
-// const nav = document.querySelector(".page-header nav");
-// const menu = document.querySelector(".page-header .menu");
-// const lottieWrapper = document.querySelector(".lottie-wrapper");
-// const lottiePlayer = document.querySelector("lottie-player");
-const scrollUp = "scroll-up";
-const scrollDown = "scroll-down";
-let lastScroll = 10;
-
-
-
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  if (currentScroll <= 0) {
-    body.classList.remove(scrollUp);
-    return;
-  }
-
-  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
-    // down
-    body.classList.remove(scrollUp);
-    body.classList.add(scrollDown);
-    // lottiePlayer.play();
-  } else if (
-    currentScroll < lastScroll &&
-    body.classList.contains(scrollDown)
-  ) {
-    // up
-    body.classList.remove(scrollDown);
-    body.classList.add(scrollUp);
-    // lottiePlayer.stop();
-  }
-  lastScroll = currentScroll;
-});
-
-
-// 
-
   return (
-    <header className='page-header'>
+    <header>
       <Container className={`${styles.innerContainer} innerContainer`}>
         <div onClick={openMenu}>
           <Image
