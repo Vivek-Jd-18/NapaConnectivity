f45e9d962271e4e34be091273802a6b47ff03f54
aaronc318 <aaronc171@gmail.com>
2022-10-14 17:36:22 +0700

    edits

--- src/components/CoBatchingPools/CoBetchingPools.tsx
+++ src/components/CoBatchingPools/CoBetchingPools.tsx
@@ -33,7 +33,7 @@ export default function CoBetchingPools() {
                             <button className="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home" type="button" role="tab" aria-controls="home" aria-selected="true">My Pools</button>
                         </li>
                         <li className="nav-item" role="presentation">
-                            <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false">Other Pools</button>
+                            <button className="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="false"> Member Pools</button>
                         </li>
                     </ul>
                 </div>                
