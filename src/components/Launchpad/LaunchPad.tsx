002b3ddf9a789310143d8a854a1c547d1de31229
brycemca03 <aaronc171@gmail.com>
2022-10-19 06:08:37 -0700

    edits

--- src/components/Launchpad/LaunchPad.tsx
+++ src/components/Launchpad/LaunchPad.tsx
@@ -11,10 +11,10 @@ export default function LaunchPad() {
     <>
         <div className={styles.LaunchPadMain}>
           <div className={styles.LeftLaunchPad}>
-            <h1 className={styles.LaunchPadDeflt}>Launchpad</h1>
+            <h1 className={styles.LaunchPadDeflt}>NAPA Launchpad</h1>
             <p>Crowdfund and launch your NFT project with NAPA Society. The NAPA launchpad tools will allow you to give you project exposure and resources to successfully launch your NFT project and showcase your art on the NAPA NFT marketplace. Submit your application for consideration!</p>
             <button className={styles.FrstBtn}>
-                Support
+                Submit New Project
                 <Image src="/img/right_arrow_help.png" alt="" width={36} height={24}  />
             </button>
           </div>
