9b4a1e25f6b06bae2b12c04ed68e62cb381a0863
Ashvin Upadala <ashvinupadala@Ashvins-MacBook-Air.local>
2022-10-27 05:25:58 -0700

    corrections

--- src/components/Settings/WhiteListComponent/WhiteListComponent.tsx
+++ src/components/Settings/WhiteListComponent/WhiteListComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Image from 'next/image';
-import { MoreIcon, NapaIcon } from '../../assets';
+import { NapaIcon } from '../../assets';
 import styles from './WhiteListComponent.module.scss';
 import { useState } from 'react';
 import DropDownComponent from '../../Dropdown/Dropdown';
