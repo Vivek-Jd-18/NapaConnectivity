efa678099c81a0df6e9c8b2146890d92649d2386
brycemca03 <aaronc171@gmail.com>
2022-11-22 09:42:22 +0700

    reverted close button change

--- src/components/SocialArt/FeedTab.tsx
+++ src/components/SocialArt/FeedTab.tsx
@@ -45,7 +45,6 @@ export default function FeedTab({ socket }: FeedTabProps) {
   const [loading, setLoading] = React.useState(false);
   const [getPostsLoading, setGetPostsLoading] = React.useState(false);
   const [posts, setPosts] = React.useState<Post[] | null>(null);
-  const [counter, setCounter] = React.useState(60);
 
   const { profileDetails } = useProfile();
 
@@ -60,13 +59,6 @@ export default function FeedTab({ socket }: FeedTabProps) {
     ev.stopPropagation();
     handleDrop(ev.dataTransfer.files[0]);
   };
-  React.useEffect(() => {
-    let timer: any;
-    if (!getPostsLoading) {
-      timer = counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-    }
-    return () => clearInterval(timer);
-  }, [counter, getPostsLoading]);
 
   const handleClick = () => {
     // @ts-ignore
@@ -466,7 +458,7 @@ export default function FeedTab({ socket }: FeedTabProps) {
                             </p>
                           </div>
                         </div>
-                        {counter > 0 && (
+                        { 0 && (
                           <div className={`${styles.messageContainer}`}>
                             Congratulations! Your post is now live for 12 hours! 👏
                           </div>
