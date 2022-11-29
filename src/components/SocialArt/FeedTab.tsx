a37d44cb20177c4f042283bb21c229e8a4529174
brycemca03 <aaronc171@gmail.com>
2022-11-29 16:03:56 +0700

    Minor change

--- src/components/SocialArt/FeedTab.tsx
+++ src/components/SocialArt/FeedTab.tsx
@@ -480,7 +480,7 @@ export default function FeedTab({ socket }: FeedTabProps) {
                           </div>
                           {counter > 0 && (
                             <div className={`${styles.messageContainer}`}>
-                              Congratulations! Your post is now live for 12
+                              Your post is now live for 12
                               hours!
                             </div>
                           )}
