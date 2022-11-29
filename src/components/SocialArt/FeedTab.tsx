2efcdc110ed7b229540f20afeb3117e1e9306f85
brycemca03 <aaronc171@gmail.com>
2022-11-29 19:29:12 +0700

    Removed lazy coding

--- src/components/SocialArt/FeedTab.tsx
+++ src/components/SocialArt/FeedTab.tsx
@@ -534,7 +534,7 @@ export default function FeedTab({ socket }: FeedTabProps) {
                   </div>
                   <div className={styles.videoPreview}>
                     <video
-                      style={{ objectFit: 'fill' }}
+                      // style={{ objectFit: 'fill' }}
                       width={'100%'}
                       height={'100%'}
                       autoPlay
