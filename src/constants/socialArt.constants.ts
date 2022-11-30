f0ac4d3d1ce27ebebcaabec0a91b1d66ad1c19a6
Malik Zulqurnain <malikzulqurnain2121@gmail.com>
2022-11-30 10:13:49 +0500

    Add messages styling

--- src/constants/socialArt.constants.ts
+++ src/constants/socialArt.constants.ts
@@ -17,7 +17,7 @@ export const socialArtMessagesTriggers = [
   {
     trigger: 'Post receives more awards than your posts normally receive',
     message:
-      'This one was a hit! List this iconic moment on the NAPA',
+      'This one was a hit! List this iconic moment on the NAPA :fireworks:',
   },
   {
     trigger: 'Users posts their first video',
