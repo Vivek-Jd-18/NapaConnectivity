2939f6dc22890c45626f467d3eb0ad6b8c5c0ed9
hammadyounas <m.hammadyounas@outlook.com>
2022-12-15 14:17:03 +0500

    code refactoring for list view and grid view in My Active SNFTs tab

--- src/components/SocialArt/MintedTabList.tsx
+++ src/components/SocialArt/MintedTabList.tsx
@@ -1,80 +1,29 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './MintedTabList.module.scss';
 import Image from 'next/image';
-import { getAllMintedPosts, updatePostMint } from '@/services/MintApi';
 import { MintPost } from '@/types/mint';
 import { FadeLoader } from 'react-spinners';
 import moment from 'moment';
-import { useRouter } from 'next/router';
-import { toast } from 'react-toastify';
-import { CustomToastWithLink } from '../CustomToast/CustomToast';
-import { ErrorIcon } from '../assets';
 import useProfile from '@/hooks/useProfile';
 import MyTimer from '../LiverTimer/Mytimer';
 
-export default function MintedTabList() {
-  const [loading, setLoading] = React.useState(false);
-  const [mintPosts, setMintPosts] = React.useState<MintPost[] | null>(null);
-  const { push } = useRouter();
-  const { profileDetails } = useProfile();
+type MintedTabListProps = {
+  loading: boolean;
+  mintPosts: MintPost[] | null;
+  getEndDate: CallableFunction;
+  Status: CallableFunction;
+  handleMintPostUpdate: CallableFunction;
+};
 
-  const handleGetMintPosts = async () => {
-    setLoading(true);
-    const { data }: any = await getAllMintedPosts();
-    setMintPosts(data?.data || []);
-    setLoading(false);
-  };
+export default function MintedTabList({
+  loading,
+  mintPosts,
+  getEndDate,
+  Status,
+  handleMintPostUpdate,
+}: MintedTabListProps) {
+  const { profileDetails, profileId } = useProfile();
 
-  useEffect(() => {
-    handleGetMintPosts();
-  }, []);
-
-  const getEndDate = (timestamp: any) => {
-    const date = new Date(timestamp).getTime();
-    const endTime = new Date(date + 12 * 3600 * 1000);
-    return moment(endTime).format('h:mm:ss a');
-  };
-
-  const handleMintPostUpdate = async (data: any) => {
-    setLoading(true);
-    const { message, error } = await updatePostMint(data);
-    setLoading(false);
-    push('/sell-nft');
-    if (error) {
-      setLoading(false);
-      toast.error(
-        CustomToastWithLink({
-          icon: ErrorIcon,
-          title: 'Error',
-          description: message,
-          time: 'Now',
-        })
-      );
-      return;
-    }
-    setLoading(false);
-  };
-  const Status = (statusVal: string) => {
-    if (statusVal == '0') {
-      return 'Live Post';
-    }
-    if (statusVal == '1') {
-      return 'Sent for Review';
-    }
-    if (statusVal == '2') {
-      return 'In Progress';
-    }
-    if (statusVal == '3') {
-      return 'Pending Further Review';
-    }
-    if (statusVal == '4') {
-      return 'Approved'
-    }
-    if(statusVal == '5'){
-      return 'Declined'
-    }
-    return;
-  };
   return (
     <div className={styles.MainListBox}>
       <div className={styles.MainListHead}>
@@ -108,160 +57,160 @@ export default function MintedTabList() {
             </div>
           ) : mintPosts?.length ? (
             <div className={`${styles.RowLabel} ${styles.RowTwo}`}>
-              {mintPosts.map((post, index) => {
-                return (
-                  <div className={styles.MainListListInn} key={index}>
-                    <div className={`${styles.RowLabel} ${styles.RowOne}`}>
-                      <Image
-                        src={post.thumbnail}
-                        alt=""
-                        width={72}
-                        height={72}
-                      />
-                      <div className={styles.RspnsvListtext}>
-                        <div className={styles.TxtH3}>
-                          <h3>{post.SNFTTitle}</h3>
+              {mintPosts
+                .filter((p) => p.profileId == profileId)
+                .map((post, index) => {
+                  return (
+                    <div className={styles.MainListListInn} key={index}>
+                      <div className={`${styles.RowLabel} ${styles.RowOne}`}>
+                        <Image
+                          src={post.thumbnail}
+                          alt=""
+                          width={72}
+                          height={72}
+                        />
+                        <div className={styles.RspnsvListtext}>
+                          <div className={styles.TxtH3}>
+                            <h3>{post.SNFTTitle}</h3>
+                          </div>
+                          <div className={styles.RspnsvEarn}>
+                            <Image
+                              src="/img/napa_icon_blue.png"
+                              alt=""
+                              width={17}
+                              height={13}
+                            />
+                            12.35
+                            <span className={styles.DefSpan}>Earned</span>
+                          </div>
                         </div>
-                        <div className={styles.RspnsvEarn}>
+                      </div>
+                      <div className={`${styles.RowLabel} ${styles.RowThree}`}>
+                        <div className={styles.Rspnsvh4}>
+                          <h4>Live Post Remaining</h4>
+                        </div>
+                        <MyTimer
+                          // @ts-ignore
+                          expiryTimestamp={new Date(post.timeMinted).setHours(
+                            new Date(post.timeMinted).getHours() + 12
+                          )}
+                        />
+                      </div>
+                      <div className={`${styles.RowLabel} ${styles.RowTwo}`}>
+                        <div className={styles.Rspnsvh4}>
+                          <h4>Date Minted</h4>
+                        </div>
+                        <h6>{moment(post.timeMinted).format('D MMM YYYY')}</h6>
+                      </div>
+                      <div className={`${styles.RowLabel} ${styles.RowThree}`}>
+                        <div className={styles.Rspnsvh4}>
+                          <h4>Live Start Date</h4>
+                        </div>
+                        <h6>{moment(post.timeMinted).format('h:mm:ss a')}</h6>
+                      </div>
+                      <div className={`${styles.RowLabel} ${styles.RowFour}`}>
+                        <div className={styles.Rspnsvh4}>
+                          <h4>Live End Date</h4>
+                        </div>
+                        <h6>{getEndDate(post.timeMinted)}</h6>
+                      </div>
+                      <div className={`${styles.RowLabel} ${styles.RowFour}`}>
+                        <div className={styles.Rspnsvh4}>
+                          <h4>Status</h4>
+                        </div>
+                        <h6>{Status(post.status)}</h6>
+                      </div>
+                      <div className={`${styles.RowLabel} ${styles.RowFive}`}>
+                        <div className={styles.Rspnsvh4}>
+                          <h4>Current Tier</h4>
+                        </div>
+                        <div className={styles.RspnsvIcnSpn}>
+                          <Image
+                            src="/img/napa_icon.png"
+                            alt=""
+                            width={17}
+                            height={13}
+                          />
+                          <h6>20.01</h6>
+                        </div>
+                      </div>
+                      <div className={`${styles.RowLabel} ${styles.RowSix}`}>
+                        <div className={styles.Rspnsvh4}>
+                          <h4>Earned</h4>
+                        </div>
+                        <div className={styles.RspnsvIcnSpn}>
                           <Image
                             src="/img/napa_icon_blue.png"
                             alt=""
                             width={17}
                             height={13}
                           />
-                          12.35
-                          <span className={styles.DefSpan}>Earned</span>
+                          <h3 className={styles.BlueH6}>12.35</h3>
                         </div>
                       </div>
-                    </div>
-                    <div className={`${styles.RowLabel} ${styles.RowThree}`}>
-                      <div className={styles.Rspnsvh4}>
-                        <h4>Live Post Remaining</h4>
-                      </div>
-                      <MyTimer
-                       // @ts-ignore
-                      expiryTimestamp={new Date(post.timeMinted).setHours(
-                        new Date(post.timeMinted).getHours() + 12
-                      )}
-                    />
-                    </div>
-                    <div className={`${styles.RowLabel} ${styles.RowTwo}`}>
-                      <div className={styles.Rspnsvh4}>
-                        <h4>Date Minted</h4>
-                      </div>
-                      <h6>{moment(post.timeMinted).format('D MMM YYYY')}</h6>
-                    </div>
-                    <div className={`${styles.RowLabel} ${styles.RowThree}`}>
-                      <div className={styles.Rspnsvh4}>
-                        <h4>Live Start Date</h4>
-                      </div>
-                      <h6>{moment(post.timeMinted).format('h:mm:ss a')}</h6>
-                    </div>
-                    <div className={`${styles.RowLabel} ${styles.RowFour}`}>
-                      <div className={styles.Rspnsvh4}>
-                        <h4>Live End Date</h4>
-                      </div>
-                      <h6>{getEndDate(post.timeMinted)}</h6>
-                    </div>
-                    <div className={`${styles.RowLabel} ${styles.RowFour}`}>
-                      <div className={styles.Rspnsvh4}>
-                        <h4>Status</h4>
-                      </div>
-                      <h6>{Status(post.status)}</h6>
-                    </div>
-                    <div className={`${styles.RowLabel} ${styles.RowFive}`}>
-                      <div className={styles.Rspnsvh4}>
-                        <h4>Current Tier</h4>
-                      </div>
-                      <div className={styles.RspnsvIcnSpn}>
-                        <Image
-                          src="/img/napa_icon.png"
-                          alt=""
-                          width={17}
-                          height={13}
-                        />
-                        <h6>20.01</h6>
-                      </div>
-                    </div>
-                    <div className={`${styles.RowLabel} ${styles.RowSix}`}>
-                      <div className={styles.Rspnsvh4}>
-                        <h4>Earned</h4>
-                      </div>
-                      <div className={styles.RspnsvIcnSpn}>
-                        <Image
-                          src="/img/napa_icon_blue.png"
-                          alt=""
-                          width={17}
-                          height={13}
-                        />
-                        <h3 className={styles.BlueH6}>12.35</h3>
+                      <div className={`${styles.RowLabel} ${styles.RowSeven}`}>
+                        <button
+                          role="button"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false"
+                        >
+                          <Image
+                            src="/img/dotet_ic.png"
+                            alt=""
+                            width={24}
+                            height={24}
+                          />
+                        </button>
+                        <ul className="dropdown-menu dropdown-menu-end drpdwn_list">
+                          <li>
+                            <a className="dropdown-item" href="#">
+                              <Image
+                                src="/img/gift_icon.png"
+                                alt=""
+                                width={20}
+                                height={20}
+                              />
+                              Redeem Tokens
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              className={`${
+                                post.status === '0' && styles.disabled
+                              } dropdown-item`}
+                              href="#"
+                              onClick={() =>
+                                handleMintPostUpdate({
+                                  mintId: post.mintId,
+                                  postId: post.postId,
+                                  profileId: profileDetails?.profileId,
+                                  marketplace_listed: 'true',
+                                })
+                              }
+                            >
+                              <Image
+                                src="/img/send_icon.png"
+                                alt=""
+                                width={20}
+                                height={20}
+                              />
+                              Submit to Marketplace
+                            </a>
+                            <a className={`dropdown-item`}>
+                              <Image
+                                src="/img/send_icon.png"
+                                alt=""
+                                width={20}
+                                height={20}
+                              />
+                              Ask Dave
+                            </a>
+                          </li>
+                        </ul>
                       </div>
                     </div>
-                    <div className={`${styles.RowLabel} ${styles.RowSeven}`}>
-                      <button
-                        role="button"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                      >
-                        <Image
-                          src="/img/dotet_ic.png"
-                          alt=""
-                          width={24}
-                          height={24}
-                        />
-                      </button>
-                      <ul className="dropdown-menu dropdown-menu-end drpdwn_list">
-                        <li>
-                          <a className="dropdown-item" href="#">
-                            <Image
-                              src="/img/gift_icon.png"
-                              alt=""
-                              width={20}
-                              height={20}
-                            />
-                            Redeem Tokens
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            className={`${
-                              post.status === '0' && styles.disabled
-                            } dropdown-item`}
-                            href="#"
-                            onClick={() =>
-                              handleMintPostUpdate({
-                                mintId: post.mintId,
-                                postId: post.postId,
-                                profileId: profileDetails?.profileId,
-                                marketplace_listed: 'true',
-                              })
-                            }
-                          >
-                            <Image
-                              src="/img/send_icon.png"
-                              alt=""
-                              width={20}
-                              height={20}
-                            />
-                            Submit to Marketplace
-                          </a>
-                          <a
-                            className={`dropdown-item`}
-                          >
-                            <Image
-                              src="/img/send_icon.png"
-                              alt=""
-                              width={20}
-                              height={20}
-                            />
-                            Ask Dave
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
             </div>
           ) : (
             <h3 className={styles.noPostFound}>No post found</h3>
--- src/components/SocialArt/SocialArtSc.tsx
+++ src/components/SocialArt/SocialArtSc.tsx
@@ -1,7 +1,7 @@
 import Container from '../../Layout/Container/Container';
 import Footer from '../Footer/Footer';
 import styles from './SocialArt.module.scss';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import FeedTab from './FeedTab';
@@ -9,12 +9,125 @@ import MintedPostsTab from './MintedPostsTab';
 import MintedTabInBox from './MintedTabInBox';
 import MintedTabList from './MintedTabList';
 import { SOCIAL_ART_WEBSOCKET_URL } from '@/constants/url';
+import { MintPost } from '@/types/mint';
+import { useRouter } from 'next/router';
+import { getAllMintedPosts, updatePostMint } from '@/services/MintApi';
+import moment from 'moment';
+import { toast } from 'react-toastify';
+import { CustomToastWithLink } from '../CustomToast/CustomToast';
+import { ErrorIcon } from '../assets';
 
 export default function SocialArtSc() {
   const socialArtSocket = new WebSocket(SOCIAL_ART_WEBSOCKET_URL);
   const [view, setView] = React.useState(true);
   const handlelistView = (val: boolean) => {
     setView(val);
+  };
+  const [loading, setLoading] = React.useState(false);
+  const [mintPosts, setMintPosts] = React.useState<MintPost[] | null>(null);
+  const { push } = useRouter();
+
+  const handleGetMintPosts = async () => {
+    setLoading(true);
+    const { data }: any = await getAllMintedPosts();
+    setMintPosts(data?.data || []);
+    setLoading(false);
+  };
+
+  const handleNewMint = (post: any) => {
+    setMintPosts((prevState) => {
+      if (prevState) {
+        const data = [post, ...prevState];
+        const filteredPost = data.filter(
+          (v, i, a) => a.findIndex((v2) => v2?.postId === v?.postId) === i
+        );
+        return [...filteredPost];
+      } else {
+        let data = [];
+        // @ts-ignore
+        data.push(post);
+        return [...data];
+      }
+    });
+  };
+
+  useEffect(() => {
+    handleGetMintPosts();
+  }, []);
+
+  useEffect(() => {
+    // @ts-ignore
+    socialArtSocket.addEventListener('message', ({ data }) => {
+      const response = JSON.parse(data);
+      if (response?.event === 'mints') {
+        handleNewMint(response?.posts);
+      } else if (response?.event === 'mint-post-status-update') {
+        handleMintPostStatusUpdate(response?.mintId, response?.mint);
+      }
+    });
+    return () => {
+      socialArtSocket.removeEventListener('message', () => {});
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleMintPostStatusUpdate = (mintId: string, mint: MintPost) => {
+    setMintPosts((prevState) => {
+      const temp = prevState ? prevState : [];
+      const index = temp.findIndex((m) => m.mintId == mintId);
+      if (index > -1) {
+        temp[index] = mint;
+      }
+      return temp;
+    });
+  };
+
+  const getEndDate = (timestamp: any) => {
+    const date = new Date(timestamp).getTime();
+    const endTime = new Date(date + 12 * 3600 * 1000);
+    return moment(endTime).format('h:mm:ss a');
+  };
+
+  const handleMintPostUpdate = async (data: any) => {
+    setLoading(true);
+    const { message, error } = await updatePostMint(data);
+    setLoading(false);
+    push('/sell-nft');
+    if (error) {
+      setLoading(false);
+      toast.error(
+        CustomToastWithLink({
+          icon: ErrorIcon,
+          title: 'Error',
+          description: message,
+          time: 'Now',
+        })
+      );
+      return;
+    }
+    setLoading(false);
+  };
+
+  const Status = (statusVal: string) => {
+    if (statusVal == '0') {
+      return 'Live Post';
+    }
+    if (statusVal == '1') {
+      return 'Sent for Review';
+    }
+    if (statusVal == '2') {
+      return 'In Progress';
+    }
+    if (statusVal == '3') {
+      return 'Pending Further Review';
+    }
+    if (statusVal == '4') {
+      return 'Approved';
+    }
+    if (statusVal == '5') {
+      return 'Declined';
+    }
+    return;
   };
   return (
     <div className={`${styles.container}`}>
@@ -38,7 +151,13 @@ export default function SocialArtSc() {
                   role="tabpanel"
                   aria-labelledby="nav-home-tab"
                 >
-                  <MintedTabInBox socket={socialArtSocket} />
+                  <MintedTabInBox
+                    loading={loading}
+                    mintPosts={mintPosts}
+                    getEndDate={getEndDate}
+                    Status={Status}
+                    handleMintPostUpdate={handleMintPostUpdate}
+                  />
                 </div>
               ) : (
                 <div
@@ -47,7 +166,13 @@ export default function SocialArtSc() {
                   role="tabpanel"
                   aria-labelledby="nav-profile-tab"
                 >
-                  <MintedTabList />
+                  <MintedTabList
+                    loading={loading}
+                    mintPosts={mintPosts}
+                    getEndDate={getEndDate}
+                    Status={Status}
+                    handleMintPostUpdate={handleMintPostUpdate}
+                  />
                 </div>
               )}
             </div>
